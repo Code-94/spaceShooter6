db3c6e3bc38b94b57647ac38f66f56d265600726
GaetanMeyer <gaetanmeyer185@gmail.com>
2025-11-14 20:50:20 +0100

    add sounds to the folder

---
+++ UI.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <SFML/Graphics/Texture.hpp>
+#include <SFML/Graphics/Text.hpp>
+#include <SFML/Graphics/RectangleShape.hpp>
+
+class UI : public sf::Drawable{
+
+private:
+	sf::Texture score_texture;
+	
+
+	sf::RectangleShape rect_;
+
+
+
+
+};
